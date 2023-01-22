ad6556c3d5f673e2d99395e75bcd73321c9bba03
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-01-22 22:09:30 +0100

    Adds more combinators.

--- Sources/SweetBool/Predicate/Predicate.swift
+++ Sources/SweetBool/Predicate/Predicate.swift
@@ -14,19 +14,35 @@ struct Predicate<A> {
 
 extension Predicate {
 
+    var not: Predicate<A> {
+        .init { (a: A) in
+            !check(a)
+        }
+    }
 
+    // self [1,2,3], other [3,4,5], result: [1,2,3,4,5]
     func or(_ other: Predicate<A>) -> Predicate<A> {
         .init { (a: A) in
             self.check( a ) || other.check( a )
         }
     }
 
+    // self [1,2,3], other [3,4,5], result: [3]
     func and(_ other: Predicate<A>) -> Predicate<A> {
         .init { (a: A) in
             self.check( a ) && other.check( a )
         }
     }
 
+    // self [1,2,3], other [3,4,5], result: [1,2]
+    func complement(_ other: Predicate<A>) -> Predicate<A> {
+        self.and( other.not )
+    }
+
+    // self [1,2,3], other [3,4,5], result: [4,5]
+    func difference(_ other: Predicate<A>) -> Predicate<A> {
+        self.not.and( other )
+    }
 }
 
 // MARK: - Optional
